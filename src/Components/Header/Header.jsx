73fd5482b8f2f7dae816711158828221c3d4ef29
talgat55 <lightx_design@yahoo.com>
2020-10-19 00:35:29 +0600

    add header wrap and layout main  and proccess fix error with bootstrap

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Row, Container, Col} from 'react-bootstrap'
+import CartImg from '../../static/cart.svg'
+
+const Header = () => {
+
+    return (
+        <HeaderComponent className="header">
+            <Container>
+                <Row>
+                    <Col xs={12} lg={8}>
+
+                    </Col>
+                    <Col className="d-flex header_cart" xs={12} lg={4}>
+                        cart
+                        {CartImg}
+                        <span className="header_cart-counter">
+                            3
+                        </span>
+                    </Col>
+                </Row>
+            </Container>
+        </HeaderComponent>
+    )
+}
+
+export default Header;
+
+const HeaderComponent = styled.header`
+  &.header{
+    &_cart{
+      position: relative;
+      .header_cart-counter{
+        position: absolute;
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        overflow: hidden;
+        background-color: #FF1876; 
+      }
+      
+    }
+  }
+`;
---
+++ src/layout/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "../Components/Header/Header";
+
+
+const Main = () => {
+
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+export default Main
